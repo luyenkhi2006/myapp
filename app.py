45263405242c708183cfa3d5bf992062cbbec934
luyenkhi2006 <dtc245180005@ictu.edu.vn>
2026-09-11 09:54:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,9 +56,7 @@ if uploaded_files:
     else:
         try:
             # Khởi tạo Vector Store với Gemini Embeddings
-            embeddings = GoogleGenerativeAIEmbeddings(
-                model="models/text-embedding-004", google_api_key=api_key
-            )
+            embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=api_key)
             vector_store = FAISS.from_documents(chunks, embeddings)
 
             st.divider()
